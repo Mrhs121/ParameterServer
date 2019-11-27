501980e1e90248fd530e611bb7f27595dc57cd0e
shenghuang <1367467330@qq.com>
2019-11-27 17:28:30 +0800

    add core package

---
+++ src/com/hs/parameterserver/core/Client.java
@@ -0,0 +1,7 @@
+package com.hs.parameterserver.core;
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Client");
+    }
+}
---
+++ src/com/hs/parameterserver/core/Server.java
@@ -0,0 +1,7 @@
+package com.hs.parameterserver.core;
+
+public class Server {
+    public static void main(String[] args) {
+        System.out.println("Server");
+    }
+}
